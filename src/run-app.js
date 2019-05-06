ea567de0a1e563da0753fd9a06c362d3da3da2e5
jangot <Pavel.Pulin@infobip.com>
2019-05-06 21:47:51 +0300

    spin

--- src/run-app.js
+++ src/run-app.js
@@ -42,8 +42,17 @@ function Road($myRow) {
 }
 
 function Spin($myRow) {
+    var $result = $myRow.find('.result');
+
+    $myRow.find('select').change(function() {
+        var somethink = $myRow.find('.some-think').val() * 1;
+        var positions = $myRow.find('.positions').val() * 1;
+
+        $result.val(somethink + positions);
+    });
+
     this.getValue = function () {
-        return 10;
+        return $result.val() || 0;
     };
 }
 
