924fb13861e91627d8b78f9a15817072f77da9a8
TimPulin <47381676+TimPulin@users.noreply.github.com>
2019-05-11 00:46:12 +0400

    все сделано

--- popupwindow.js
+++ popupwindow.js
@@ -4,11 +4,10 @@
 	//---------------мониторинг изменения значения селекта для блокировки/разблокировки селектов------------
 	$(document).ready(function(){
 		var Iam
-		var value_of_selected
+		
 		$("div.JSmodalWindow select").not(".JSSecondarySelect").change (function(){
 			Iam=$(this);
-			value_of_selected=Iam.val();
-			if (value_of_selected==0){
+			if (Iam.val()==0){
 				LockNextSelect(Iam)
 				LockAllSelect(Iam);
 			} else {
@@ -94,10 +93,8 @@
 
 		//---------разблокировка кнопки "добавить прыжок"
 		$(".unlockBtnAddJump").change(function(){
-			let Iam_btn_add_jump=$(this);
-			let value_of_selected=Iam_btn_add_jump.val();
-			if (value_of_selected!=0){
-				Iam_btn_add_jump.parent().parent().next().prop("disabled", false);
+			if ($(this).val()!=0){
+				$(this).parent().parent().next().prop("disabled", false);
 			};
 		}); //КОНЕЦ разблокировки
 
@@ -108,64 +105,58 @@
 
 		//--------------мониторинг изменения значения селектов для показа имени
 		$("div.contanerGrey select").change(function(){
-			let Iam_select=$(this);
-			let value_of_selected=Iam_select.val();
-			if (value_of_selected==0){
-				ShowNameOfSelect(Iam_select);
+			if ($(this).val()==0){
+				$(this).prop("selectedIndex","0")
 			};
 		});//КОНЕЦ мониторинга для показа имени
-
-		//функция показа имени селекта
-		function ShowNameOfSelect(Iam_select){Iam_select.prop("selectedIndex","0")};
-	});//----------------------------КОНЕЦ блока поведения кнопок
-
-
-	//БЛОК СЕЛЕКТА С ДОРОЖКАМИ
-	$(document).ready(function(){
+		
+		//мониторинг изменения селекта с дорожками
 		$(".JSselectStep").change(function(){
-			value_of_selected=$(this).val();
-			if (value_of_selected==="ChSq"){
+			if ($(this).val()==="ChSq"){
 				$(this).next().prop("selectedIndex", "3").prop("disabled", true);
 			};
 		});
-	});//КОНЕЦ блока с дорожками
+	});//----------------------------КОНЕЦ блока поведения кнопок
 
 
 	//БЛОК РАБОТЫ С ТРЕТЬИМ ПРЫЖКОМ В КАСКАДЕ
 	$(document).ready(function(){
+		
+		var Iam;
+		var Iam_SelectFloat;
 		//--------------мониторинг изменения значения селекта для формирования списка возможного третьего прыжка в каскаде-------------
 		$(".JSSelectChanger").change(function(){
-			var Iam=$(this);
-			let Iam_SelectFloat=Iam.parent().parent().parent().next().find(".JSSelectFloatOptions");
+			Iam=$(this);
+			Iam_SelectFloat=Iam.parent().parent().parent().next().find(".JSSelectFloatOptions");
 			let value_of_selected=Iam.val();
-			if (value_of_selected==="A"){
+			if ($(this).val()==="A"){
 				LockAllSelect(Iam)
 				Iam.prev().prop("disabled",false);
-			} else if (value_of_selected==="Eu"){
-				AddOptionsForEu(Iam, Iam_SelectFloat)
-			} else {AddOptionsForLoT(Iam, Iam_SelectFloat)};
+			} else if ($(this).val()==="Eu"){
+				AddOptionsForEu()
+			} else {AddOptionsForLoT()};
 		});//КОНЕЦ мониторинга для определения третьего прыжка
 
 		//функция изменения options в третьем прыжке каскада для оллера
-		function AddOptionsForEu(Iam, Iam_SelectFloat){
+		function AddOptionsForEu(){
 			Iam.prev().prop("selectedIndex","2").prop("disabled",true);
-			ShowNamesThirdRow(Iam_SelectFloat);
+			ShowNamesThirdRow();
 			$.each(arrOptionsForEu, function(value, key){
 				Iam_SelectFloat.append('<option value="'+value+'">'+key+'</option>')
 			});//КОНЕЦ each()
-		};//КОНЕЦ AddOptionsForEu(Iam)
+		};//КОНЕЦ AddOptionsForEu()
 
 		//функция изменения options в третьем прыжке каскада для тулупа и ритбергера
-		function AddOptionsForLoT(Iam, Iam_SelectFloat){
+		function AddOptionsForLoT(){
 			Iam.prev().prop("disabled",false)
-			ShowNamesThirdRow(Iam_SelectFloat);
+			ShowNamesThirdRow();
 			$.each(arrOptionsForLoT, function(value, key){
 				Iam_SelectFloat.append('<option value="'+value+'">'+key+'</option>')
 			});//КОНЕЦ each()
-		};//КОНЕЦ AddOptionsForLoT(Iam)
+		};//КОНЕЦ AddOptionsForLoT()
 
 		//показ имени селектов в третьем ряду, сброс выпадающего списка в селекте "прыжки"
-		function ShowNamesThirdRow(Iam_SelectFloat){
+		function ShowNamesThirdRow(){
 			Iam_SelectFloat.find("option").not("option[value=0]").remove();
 			Iam_SelectFloat.parent().find("select").prop("selectedIndex", "0")
 			return
@@ -175,23 +166,23 @@
 
 	//БЛОК СЕЛЕКТА С Е и V
 	$(document).ready(function(){
+		
+		var value_of_selected;
+		
 		$(".JSNeedAdditionalSelect").change(function(){
-			Iam_need_e=$(this);
-			let value_of_selected=Iam_need_e.val();
-			CheckForAdditionalSelect(value_of_selected);
-			if (CheckForAdditionalSelect(value_of_selected)){
-				Iam_need_e.parent().find("select").last().show().css("color","#999").prop("selectedIndex", "0");
-			} else{Iam_need_e.parent().find("select").last().prop("selectedIndex", "0").hide()};
+			value_of_selected=$(this).val();
+			CheckForAdditionalSelect();
+			if (CheckForAdditionalSelect()){
+				$(this).parent().find("select").last().show().css("color","#999").prop("selectedIndex", "0");
+			} else{$(this).parent().find("select").last().prop("selectedIndex", "0").hide()};
 		});
 		$(".JSselectAdditional").change(function(){
-			Iam_e=$(this);
-			let value_of_selected=Iam_e.val();
-			if (value_of_selected==0||value_of_selected==null){
-				Iam_e.css("color","#999");
-			}else{Iam_e.css("color","#333")}
+			if ($(this).val()==0||$(this).val()==null){
+				$(this).css("color","#999");
+			}else{$(this).css("color","#333")}
 		});
 
-		function CheckForAdditionalSelect(value_of_selected){
+		function CheckForAdditionalSelect(){
 			for (let i=0; i<arrForAdditionalSelect.length; i++){
 				if (arrForAdditionalSelect[i]===value_of_selected){
 					return true
@@ -200,53 +191,93 @@
 			return false
 		};
 	});//КОНЕЦ блока с Е и V
+	
+	//БЛОК ПОКАЗА И СКРЫТИЯ ЧЕКБОКСА "вторая половина программы"
+	$(document).ready(function(){
+
+		var Iam_click_in_main_window_forShowAndHide;
+		var ID_greyStripe_forShowAndHide;
+		
+		var value_of_select1;
+		var value_of_select2;
+		
+		//-----------мониторинг поля вызова всплывающего окна
+		$(".contanerTransparent").click(function(){
+			Iam_click_in_main_window_forShowAndHide=$(this);
+			GetIDForShowAndHide();
+		});//КОНЕЦ мониторинга поля вызова всплывающего окна
+
+		function GetIDForShowAndHide(){
+			ID_greyStripe_forShowAndHide=Iam_click_in_main_window_forShowAndHide.closest(".greyStripe").attr("id");
+			return;
+		};
+
+		//мониторинг кнопки "сохранить"
+		$(".buttonSave").click(function(){CheckIfElementIsJump()});
+		//мониторинг кнопки "сброс"
+		$(".JSbuttonReset_Modal").click(function(){HideCheckbox_SecondHalf()});
+
+		//функция проверки на выбор прыжкового элемента
+		function CheckIfElementIsJump(){
+			value_of_select1=$("div.JSjumps select.JSNeedAdditionalSelect").val();
+			value_of_select2=$("div.JSjumps select.JSNeedAdditionalSelect").prev().val();
+			if(value_of_select1!=null && value_of_select2!=null){
+				ShowCheckbox_SecondHalf();
+			}
+			else{HideCheckbox_SecondHalf()};
+		};//конец функции проверки на выбор прыжкового элемента
+
+		function ShowCheckbox_SecondHalf(){
+			$("#"+ID_greyStripe_forShowAndHide).find(".JScheckboxSecondPart").show();
+		};
+		function HideCheckbox_SecondHalf(){
+			$("#"+ID_greyStripe_forShowAndHide).find(".JScheckboxSecondPart").prop("checked", false).hide();
+		};
+		
+		//----------БЛОК СБРОСА ВСЕХ ЭЛЕМЕНОВ-----------
+		//мониторинг кнопки "сброс" в главном окне
+		$(".JSbuttonReset_Main").click(function(){
+			$(".JScheckboxSecondPart").prop("checked", false).hide();
+		});
+		
+	});//КОНЕЦ блока показа и скрытия чекбокса "вторая половина программы"
 	//КОНЕЦ скриптов всплывающего окна
 
 
 
 
 
-	//СКРИПТЫ СВЯЗИ С ВСПЛЫВАЮЩИМ ОКНОМ (с вычислительной частью)
+	//БЛОК ВЫЧИСЛЕНИЙ
 	$(document).ready(function(){
 
 		var ID_greyStripe_forCounting;
 		var arrValueAndNameOfElement={};
+		var arrRealValueOfElement={};
 		var name_of_property_for_main_counting;
 		var value_of_property_for_main_counting;
-		var Iam_click_in_main_window_forCounting;
-
+		var Iam_click_in_main_window;
+		var value_of_element;
+		var name_of_element;
+		var total_value=0;
+		var key;
+		
 		//переменные для Calculation()
 		var arrOfItems=[];
 		var item;
 		var itemLow;
 		var score;
-		var score_sub;
+		var score_of_element_in_modal;
 		var option_of_select;
 		var Iam;
 		var Iam_for_Calculation;
-
+		
+		//ВЫЧИСЛЕНИЯ В ГЛАВНОМ ОКНЕ
 		//-----------мониторинг поля для вызова всплывающего окна
-		$(".contanerTransparent").click(function(event){
-			Iam_click_in_main_window_forCounting=$(this);
+		$(".contanerTransparent").click(function(){
+			Iam_click_in_main_window=$(this);
 			GetID();
 			$("#windowElement").modal();
 		});//КОНЕЦ мониторинга поля для вызова всплывающего окна
-
-		function GetID(){
-			ID_greyStripe_forCounting=Iam_click_in_main_window_forCounting.closest(".greyStripe").attr("id");
-			return;
-		};
-
-		//-----------мониторинг селектов----------
-		$("div.JSmodalWindow select").change(function(){
-			Iam_for_Calculation=$(this).parent().parent().parent().parent();
-			DirectorModalWindow();
-		});//КОНЕЦ мониторинга селектов
-
-		function DirectorModalWindow(){
-			Calculation();
-			ShowItemAndScoreInPopup();
-		};
 
 		//мониторинг кнопки "сохранить"
 		$(".buttonSave").click(function(){DirectorBetweenMainAndModalWindow()});
@@ -256,57 +287,124 @@
 		function DirectorBetweenMainAndModalWindow(){
 			Calculation();
 			PushInto_arrValueAndNameOfElement();
-			PrintIn();
+			DirectorMainWindow();
+		};
+		
+		//мониторинг чекбокса "вторая половина программы"
+		$(".JScheckboxSecondPart").click(function(){
+			Iam_click_in_main_window=$(this);
+			GetID();
+			DirectorMainWindow();
+		});
+		
+		function DirectorMainWindow(){
+			MakeTheName();
+			value_of_element=arrValueAndNameOfElement[value_of_property_for_main_counting];
+			name_of_element=arrValueAndNameOfElement[name_of_property_for_main_counting];
+			//LetsCountGOE();
+			BonusForSecondPart();
+			PrintInForElement();
+			PushInto_arrRealValueOfElement();
+			TotalValueOfProgram();
+			PrintInForProgram();
 			ResetVar();
 		};
+		
+		//добавление бонуса за исполнения прыжка во второй половине программы
+		function BonusForSecondPart(){
+			if($("#"+ID_greyStripe_forCounting).find(".JScheckboxSecondPart").prop("checked")){
+				value_of_element=value_of_element*1.1;
+				return;
+			};
+			return;
+		};
 
-		//функция формирования объекта arrValueAndNameOfElement с названием элемента и его стоимостью
+		//вывод названия элемента и его стоимости на главную страницу
+		function PrintInForElement(){
+			$("#"+ID_greyStripe_forCounting).find(".JSoutputTransparent_NameOfElement").text(name_of_element);
+			$("#"+ID_greyStripe_forCounting).find(".JSoutputTransparent_ValueOfElement").text(value_of_element.toFixed(2));
+			return;
+		};
+		
+		//запись реальной стоимости элемента в объект
+		function PushInto_arrRealValueOfElement(){
+			arrRealValueOfElement[value_of_property_for_main_counting]=value_of_element;
+			return;
+		};
+		
+		//вычисление полной стоимости программы
+		function TotalValueOfProgram(){
+			for (key in arrRealValueOfElement){
+				total_value=total_value+arrRealValueOfElement[key];
+			};
+			return;
+		};
+		
+		//вывод полной стоимости программы на главную страницу
+		function PrintInForProgram(){
+			$("#outTotal").text(total_value.toFixed(2));
+		};
+		
+		//функция получения ID
+		function GetID(){
+			ID_greyStripe_forCounting=Iam_click_in_main_window.closest(".greyStripe").attr("id");
+			return;
+		};
+		
+		//функция формирования объекта arrValueAndNameOfElement с названием элемента и его базовой стоимостью
 		function PushInto_arrValueAndNameOfElement(){
 			MakeTheName();
 			arrValueAndNameOfElement[name_of_property_for_main_counting]=arrOfItems.join("");
-			arrValueAndNameOfElement[value_of_property_for_main_counting]=score_sub;
+			arrValueAndNameOfElement[value_of_property_for_main_counting]=score_of_element_in_modal;
 			return;
 		};
 
 		//функция формирования имени свойства объекта arrValueAndNameOfElement
 		function MakeTheName(){
-			name_of_property_for_main_counting=ID_greyStripe_forCounting;
-			name_of_property_for_main_counting=name_of_property_for_main_counting+"name";
-			value_of_property_for_main_counting=ID_greyStripe_forCounting;
-			value_of_property_for_main_counting=value_of_property_for_main_counting+"value";
+			name_of_property_for_main_counting=ID_greyStripe_forCounting+"name";
+			value_of_property_for_main_counting=ID_greyStripe_forCounting+"value";
 			return;
 		};
 
-		//функция вывода названия элемента и стоимости на главную страницу
-		function PrintIn(){
-			$("#"+ID_greyStripe_forCounting).find(".JSoutputTransparent_NameOfElement").text(arrValueAndNameOfElement[name_of_property_for_main_counting]);
-			$("#"+ID_greyStripe_forCounting).find(".JSoutputTransparent_ValueOfElement").text(arrValueAndNameOfElement[value_of_property_for_main_counting].toFixed(2));
-			return;
-		};
-
-		//функция сброса переменных
+		//сброс переменных
 		function ResetVar(){
 			ID_greyStripe_forCounting=null;
 			CleanUp_arrOfItems();
 			item="";
 			score=0;
-			score_sub=0;
+			score_of_element_in_modal=0;
+			value_of_element=0;
+			name_of_element="";
+			total_value=0;
 			Iam=null;
 			return;
 		};
 
-		//функция очистки массива набранных элементов в всплывающем окне
+		//очистка массива набранных элементов в всплывающем окне
 		function CleanUp_arrOfItems(){
 			arrOfItems.splice(0, arrOfItems.length);
 			return
 		};//КОНЕЦ CleanUp_arrOfItems
+		//КОНЕЦ вычислений в главном окне
 
 
-		//ВЫЧИСЛИТЕЛЬНАЯ ЧАСТЬ
-		//----------------функция подсчета--------------
+		//---------ФУНКЦИИ ВЫЧИСЛЕНИЯ В ВСПЛЫВАЮЩЕМ ОКНЕ--------
+		
+		//мониторинг селектов
+		$("div.JSmodalWindow select").change(function(){
+			Iam_for_Calculation=$(this).parent().parent().parent().parent();
+			DirectorModalWindow();
+		});//КОНЕЦ мониторинга селектов
+
+		function DirectorModalWindow(){
+			Calculation();
+			ShowItemAndScoreInModalWindow();
+		};
+		
+		//функция подсчета
 		function Calculation(){
 
-			score_sub=0;
+			score_of_element_in_modal=0;
 			CleanUp_arrOfItems(arrOfItems);
 			if(Iam_for_Calculation==undefined){return};
 
@@ -330,12 +428,12 @@
 					item=null;
 				};
 
-				score_sub=score_sub+score;
+				score_of_element_in_modal=score_of_element_in_modal+score;
 
 				if (index==1 && item!=null){ // отсчет второго элемента для комбинации прыжков
 					CheckAxels(itemLow);
 					if (CheckAxels(itemLow)){
-						score_sub=score_sub*0.8;
+						score_of_element_in_modal=score_of_element_in_modal*0.8;
 						item=item+"+SEQ";
 					};
 				};//КОНЕЦ отсчет второго элемента для комбинации прыжков
@@ -350,11 +448,11 @@
 		};//КОНЕЦ Calculation()
 
 		//функция вывода названия элемента и его стоимости в вверхнюю строчку всплывающего окна
-		function ShowItemAndScoreInPopup(){
+		function ShowItemAndScoreInModalWindow(){
 			Iam_for_Calculation.parent().find(".outputTitle_NameOfElement").text(arrOfItems.join(""));
-			Iam_for_Calculation.parent().find(".outputTitle_ValueOfElement").text(score_sub.toFixed(2));
+			Iam_for_Calculation.parent().find(".outputTitle_ValueOfElement").text(score_of_element_in_modal.toFixed(2));
 			return;
-		};//КОНЕЦ ShowItemAndScoreInPopup()
+		};//КОНЕЦ ShowItemAndScoreInModalWindow()
 
 		//функция проверки второго элемента - является ли он акселем
 		function CheckAxels(itemLow){
@@ -366,8 +464,28 @@
 			return false;
 		};//КОНЕЦ CheckAxels(itemLow)
 
-		//КОНЕЦ БЛОКА ВЫЧИСЛИТЕЛЬНОЙ ЧАСТИ----------------------------
-	});//КОНЕЦ СКРИПТОВ СВЯЗИ С ВСПЛЫВАЮЩИМ ОКНОМ (с вычислительной частью)
+		//КОНЕЦ функций вычисления в всплывающем окне
+		
+		//----------БЛОК СБРОСА ВСЕХ ЭЛЕМЕНОВ-----------
+		//мониторинг кнопки "сброс" в главном окне
+		$(".JSbuttonReset_Main").click(function(){
+			TotalResetInMain();
+		});
+		
+		function TotalResetInMain(){
+			ResetVar();
+			for (key in arrValueAndNameOfElement){
+				delete arrValueAndNameOfElement[key];
+			};
+			for (key in arrRealValueOfElement){
+				delete arrRealValueOfElement[key];
+			};
+			$(".JSoutputTransparent_NameOfElement").text(name_of_element);
+			$(".JSoutputTransparent_ValueOfElement").text(value_of_element.toFixed(2));
+			PrintInForProgram();
+		};
+		//КОНЕЦ блока сброса всех элементов
+	});//КОНЕЦ блока вычислений
 
 
 
@@ -480,65 +598,29 @@
 			name_of_property_forSetting=name_of_property_forSetting+index;
 			return;
 		};//КОНЕЦ MakeTheName_ForSetting
+		
+		
+		//----------БЛОК СБРОСА ВСЕХ ЭЛЕМЕНОВ-----------
+		//мониторинг кнопки "сброс" в главном окне
+		$(".JSbuttonReset_Main").click(function(){
+			TotalResetInModal();
+		});
+		function TotalResetInModal(){
+			for (key in arrListOfIndexForSelect){
+					delete arrListOfIndexForSelect[key];
+				};
+				for (key in arrListOfHideForDisplayElements){
+					delete arrListOfHideForDisplayElements[key];
+				};
+				for (key in arrListOfHideForDisplayJumps){
+				delete arrListOfHideForDisplayJumps[key];
+			};
+		};		
+		//КОНЕЦ блока сброса всех элементов
 	});//КОНЕЦ блока запоминания выбора селектов в всплывающем окне
 
 
-	//БЛОК ПОКАЗА И СКРЫТИЯ ЧЕКБОКСА "вторая половина программы"
-	$(document).ready(function(){
-
-		var Iam_click_in_main_window_forShowAndHide;
-		var ID_greyStripe_forShowAndHide;
-		var display_for_ShowAndHide;
-
-		//-----------мониторинг поля вызова всплывающего окна
-		$(".contanerTransparent").click(function(){
-			Iam_click_in_main_window_forShowAndHide=$(this);
-			GetIDForShowAndHide();
-		});//КОНЕЦ мониторинга поля вызова всплывающего окна
-
-		function GetIDForShowAndHide(){
-			ID_greyStripe_forShowAndHide=Iam_click_in_main_window_forShowAndHide.closest(".greyStripe").attr("id");
-			return;
-		};
-
-		//мониторинг кнопки "сохранить"
-		$(".buttonSave").click(function(){CheckIfElementIsJump()});
-		//мониторинг кнопки "сброс"
-		$(".JSbuttonReset_Modal").click(function(){CheckIfElementIsJump()});
-
-		//функция проверки на выбор прыжкового элемента
-		function CheckIfElementIsJump(){
-			display_for_ShowAndHide=$(".modal-content").find(".JSjumps").css("display");
-			if(display_for_ShowAndHide=="block"){
-				ShowCheckbox_SecondHalf();
-			}
-			else{HideCheckbox_SecondHalf()};
-		};//конец функции проверки на выбор прыжкового элемента
-
-		function ShowCheckbox_SecondHalf(){
-			$("#"+ID_greyStripe_forShowAndHide).find(".JScheckboxSecondPart").show();
-		};
-		function HideCheckbox_SecondHalf(){
-			$("#"+ID_greyStripe_forShowAndHide).find(".JScheckboxSecondPart").prop("checked", false).hide();
-		};
-	});//КОНЕЦ блока показа и скрытия чекбокса "вторая половина программы"
 
 
-	//БЛОК УМНОЖЕНИЯ СТОИМОСТИ ПРЫЖКА ЗА ИСПОЛНЕНИЕ ВО ВТОРОЙ ПОЛОВИНЕ ПРОГРАММЫ
-	$(document).ready(function(){
 
-		var Iam_checkbox_secondpart;
-
-		$(".JScheckboxSecondPart").click(function(){
-			Iam_checkbox_secondpart=$(this);
-			BonusForSecondPart();
-		});
-		function BonusForSecondPart(){
-			if(Iam_checkbox_secondpart.prop("checked")){
-					Iam_for_bonus=$(this).parent().parent().find("output.JSoutputTransparent_ValueOfElement");
-					val_of_output=Iam_for_bonus.val()*1.1;
-					Iam_for_bonus.val(val_of_output.toFixed(2));
-				};
-
-		};
-	});//КОНЕЦ блока умножения
+	
