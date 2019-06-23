92d878eda5de35bedebb52f0f99a21e3841a01c4
jangot <Pavel.Pulin@infobip.com>
2019-06-23 21:25:41 +0300

    core spin

--- src/elements/spin.js
+++ src/elements/spin.js
@@ -1,12 +1,51 @@
+
+
 function Spin($myRow) {
+    var SpinClass = fCalculator.Spin;
+
     var $result = $myRow.find('.result');
+    var $f = $myRow.find('[name="f"]');
+    var $type = $myRow.find('select[name="type"]');
+    var $level = $myRow.find('select[name="level"]');
+    var $error = $myRow.find('[name="error"]');
 
-    $myRow.find('select').change(function() {
-        var somethink = $myRow.find('.some-think').val() * 1;
-        var positions = $myRow.find('.positions').val() * 1;
+    var spin = fCalculator.create().spin();
 
-        $result.val(somethink + positions);
-    });
+    spin
+        .getAvailableTypes()
+        .forEach(function(type) {
+            $type.append('<option>' + type + '</option>');
+        });
+
+    spin
+        .getAvailableLevels()
+        .forEach(function(level) {
+            $level.append('<option>' + level + '</option>');
+        });
+
+    $f.change(updateValue);
+    $type.change(updateValue);
+    $level.change(updateValue);
+    $error.change(updateValue);
+
+    updateValue();
+
+    function updateValue() {
+        spin
+            .setType($type.val())
+            .setLevel($level.val())
+            .setF($f[0].checked ? SpinClass.F : '');
+
+        if (spin.hasError()) {
+            $error.parents('.form-check').show();
+            spin.setError($error[0].checked ? SpinClass.V : '');
+        } else {
+            $error.parents('.form-check').hide();
+            spin.setError('')
+        }
+
+        $result.val(spin.getValue());
+    }
 
     this.getValue = function () {
         return $result.val() || 0;
